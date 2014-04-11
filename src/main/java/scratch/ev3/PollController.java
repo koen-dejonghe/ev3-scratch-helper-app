d3f6fd86a40be676683444b2393296a6beb84afe
Koen Dejonghe <koen@fietsoverland.com>
2014-04-11 17:02:45 +0200

    update doc

--- src/main/java/scratch/ev3/PollController.java
+++ src/main/java/scratch/ev3/PollController.java
@@ -2,43 +2,41 @@ package scratch.ev3;
 
 import java.rmi.RemoteException;
 
+import lejos.hardware.DeviceException;
 import lejos.remote.ev3.RMISampleProvider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PollController {
 	
+	private static final Logger L = LoggerFactory.getLogger(PollController.class);
+	
 	@Autowired
 	RMISampleProvider distanceSampleProvider;
 	
-	@RequestMapping("/greeting")
-	public String greeting(
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
-
 	@RequestMapping("/poll")
 	public String poll(Model model) {
 
-		float distance = 0;
-		
-		try {
-			float[] fetchSample = distanceSampleProvider.fetchSample();
-			distance = fetchSample[0];
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		float distance = getSample(distanceSampleProvider);
 
 		model.addAttribute("distance", distance);
 		return "poll";
+	}
+	
+	private float getSample(RMISampleProvider provider){
+		try {
+			float[] fetchSample = provider.fetchSample();
+			return fetchSample[0];
+		} catch (RemoteException | NullPointerException | DeviceException e) {			
+			L.error(e.getMessage());
+			return 0;
+		}		
 	}
 
 }
