43f5af97a02f71ea66f5b43e2e839cd4f8146cf4
Koen Dejonghe <koen@fietsoverland.com>
2014-04-11 15:18:14 +0200

    externalized config

--- src/main/java/scratch/ev3/CoreConfig.java
+++ src/main/java/scratch/ev3/CoreConfig.java
@@ -7,17 +7,24 @@ import java.rmi.RemoteException;
 import lejos.remote.ev3.RMISampleProvider;
 import lejos.remote.ev3.RemoteEV3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CoreConfig {
 
+	@Value("${ev3.ip.address}")
+    private String ev3IpAddress;
+	
+	@Value("${ev3.sensor.distance.port}")
+    private String ev3SensorDistancePort;
+		
 	@Bean
 	public RemoteEV3 ev3(){
 		RemoteEV3 ev3 = null;
 		try {
-			ev3 = new RemoteEV3("192.168.1.144");
+			ev3 = new RemoteEV3(ev3IpAddress);
 		} catch (RemoteException | MalformedURLException | NotBoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -28,7 +35,7 @@ public class CoreConfig {
 	@Bean
 	public RMISampleProvider distanceSampleProvider(RemoteEV3 ev3) {
 
-		RMISampleProvider sp = ev3.createSampleProvider("S2",
+		RMISampleProvider sp = ev3.createSampleProvider(ev3SensorDistancePort,
 				"lejos.hardware.sensor.EV3IRSensor", "Distance");
 		return sp;
 
