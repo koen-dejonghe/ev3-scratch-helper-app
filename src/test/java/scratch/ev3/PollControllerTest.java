3e1931e4336e78311d43a51092cb771481ed792d
emdejonko <koen.dejonghe@atos.net>
2014-05-12 14:09:38 +0100

    start implementation of 1st test

--- src/test/java/scratch/ev3/PollControllerTest.java
+++ src/test/java/scratch/ev3/PollControllerTest.java
@@ -1,17 +1,51 @@
 package scratch.ev3;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriver;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes={TestConfig.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@ActiveProfiles("mockProfile")
+@WebAppConfiguration
 public class PollControllerTest {
 
+	@Autowired
+	private WebApplicationContext context;
+	
+	@Autowired
+	RemoteEV3Inf ev3;
 
-    //@Test
-    public void test() {
-        // assertions against myBean ...
-    }
+	private WebDriver driver;
+
+	@Before
+	public void setup() {
+		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		driver = new MockMvcHtmlUnitDriver(mockMvc, true);
+	}
+
+	@After
+	public void destroy() {
+		if (driver != null) {
+			driver.close();
+		}
+	}
+
+	@Test
+	public void test() {
+		driver.get("http://localhost:4321/poll");
+		String pageSource = driver.getPageSource();
+		
+	}
 }
-
