90b963105b112e3e1c4416eb9d59778c04d8919a
Koen Dejonghe <koen@fietsoverland.com>
2014-05-13 22:13:04 +0200

    tmp work

--- src/test/java/scratch/ev3/PollControllerTest.java
+++ src/test/java/scratch/ev3/PollControllerTest.java
@@ -5,14 +5,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriver;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,8 +29,9 @@ public class PollControllerTest {
 
 	@Before
 	public void setup() {
-		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-		driver = new MockMvcHtmlUnitDriver(mockMvc, true);
+		//MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		//driver = new MockMvcHtmlUnitDriver(mockMvc, true);
+		driver = new HtmlUnitDriver();
 	}
 
 	@After
@@ -45,7 +44,16 @@ public class PollControllerTest {
 	@Test
 	public void test() {
 		driver.get("http://localhost:4321/poll");
-		String pageSource = driver.getPageSource();
+		String pageSource = driver.getPageSource();				
+		System.out.println(pageSource);
+		System.out.println(driver.getCurrentUrl());
+		
+		try {
+			Thread.sleep(10000000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
